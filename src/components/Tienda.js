88f32b73ba153f1bd7bbf39160b55678e7efc3e9
Santiago Rodriguez <santir9515@gmail.com>
2022-12-11 22:05:59 -0300

    correcion errores 3

--- src/components/Tienda.js
+++ src/components/Tienda.js
@@ -1,14 +1,14 @@
-const Tienda = (props) => {
-    return (
-      <>
-      <hr></hr>
-      <h3>Telefono agregados</h3>
-      <div>Fabricante: <span>{props.marca}</span></div>
-      <div>Modelo: <span>{props.modelo}</span></div>
-      <div>Precio: <span>{props.precio}</span></div>
-      </>
+// const Tienda = (props) => {
+//     return (
+//       <>
+//       <hr></hr>
+//       <h3>Telefono agregados</h3>
+//       <div>Fabricante: <span>{props.marca}</span></div>
+//       <div>Modelo: <span>{props.modelo}</span></div>
+//       <div>Precio: <span>{props.precio}</span></div>
+//       </>
   
-    )
-  }
+//     )
+//   }
 
-  export default Tienda;
+//   export default Tienda;
